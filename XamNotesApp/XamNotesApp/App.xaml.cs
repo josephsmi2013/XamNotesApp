aca0d884c86df6e994a1645a5b0ea81aaeb1564d
Joseph Smith <josephsmi2013@gmail.com>
2020-12-25 01:02:10 -0700

    Add project files.

---
+++ XamNotesApp/XamNotesApp/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms;
+
+namespace XamNotesApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPageView());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
